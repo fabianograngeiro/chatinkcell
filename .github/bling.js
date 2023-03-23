f11357e21d60982a4a88b3f66e1f6a75d49e2346
whatsbot <117778378+fabianograngeiro@users.noreply.github.com>
2023-03-23 00:12:32 -0300

    Update bling.js

--- .github/bling.js
+++ .github/bling.js
@@ -1,30 +1,33 @@
-const request = require('request');
-const xml2js = require('xml2js');
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const PORT = process.env.PORT || 5000;
 
-function getStatusOrdemServico(numeroOrdemServico) {
-    // Monta a URL de chamada à API do Bling
-    const apiKey = 'b7eeeaa9513217cafe7daf5ad5fc7be83d28e2d861b89076ff6a1b7782af793877693bc2';
-    const url = `https://bling.com.br/Api/v2/ordensservico/${numeroOrdemServico}/json/?apikey=${apiKey}`;
+app.use(express.json());
 
-    // Faz a chamada à API do Bling usando o módulo request
-    request(url, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            // Converte a resposta XML em um objeto JavaScript usando o módulo xml2js
-            xml2js.parseString(body, function(err, result) {
-                if (!err) {
-                    // Extrai o status da ordem de serviço do objeto JavaScript retornado pela API
-                    const status = result.retorno.ordens[0].ordem[0].situacao[0].situacao[0];
+app.post('/webhook', async (req, res) => {
+  const intentName = req.body.queryResult.intent.displayName;
+  
+  if (intentName === 'Saber do meu notebook') {
+    const userId = req.body.originalDetectIntentRequest.payload.data.source.userId;
+    const blingApiKey = 'b7eeeaa9513217cafe7daf5ad5fc7be83d28e2d861b89076ff6a1b7782af793877693bc2'; // Substitua com sua API Key do Bling
+    const serviceStatus = await getServiceStatus(blingApiKey, userId);
+    const message = `O status do seu notebook é ${serviceStatus}. Para mais informações, acesse: https://www.meuservico.com/status`;
 
-                    // Retorna o status da ordem de serviço
-                    return status;
-                } else {
-                    // Trata o erro de conversão do XML para JavaScript
-                    console.log(err);
-                }
-            });
-        } else {
-            // Trata o erro de chamada à API do Bling
-            console.log(error);
-        }
+    res.json({
+      fulfillmentText: message
     });
+  }
+});
+
+async function getServiceStatus(apiKey, userId) {
+  // Use a API do Bling para buscar o status do serviço
+  const response = await axios.get(`https://bling.com.br/api/v2/servico/status?apikey=${apiKey}&userId=${userId}`);
+  const status = response.data.status;
+
+  return status;
 }
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+})
